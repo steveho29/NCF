5e48bd8a5df9df7dac676388da5b9d4f9add9ff0
Đức <64053044+steveho29@users.noreply.github.com>
2021-12-24 21:49:21 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
-echo "
-[general]n
-email = "hongocminhduc29@gmail.com"n
-" > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
+
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
